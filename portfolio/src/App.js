5b52260b3133c805f409691181a8d93af85e285a
tseloteyonas <nomiyonas21@gmail.com>
2024-07-11 04:41:18 -0700

    first commit

---
+++ portfolio/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import Header from './components/header/Header';
+import Home from './components/Home/Home';
+
+function App() {
+  return (
+    <div>
+      <Header />
+
+      <main className='main'>
+      <Home/>
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ portfolio/src/components/Home/Home.jsx
@@ -0,0 +1,17 @@
+import React from "react"
+
+const Home= () => {
+  return (
+    <div>
+      <section className="home section" id="home">
+        <div className="home_container container grid">
+            <div className="home_content grid">
+                
+            </div>
+        </div>
+      </section>
+    </div>
+  )
+};
+
+export default Home;
