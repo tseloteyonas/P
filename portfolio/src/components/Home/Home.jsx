e1a4c98ab07c25d74bfcb4ed00e9023c509be719
tseloteyonas <nomiyonas21@gmail.com>
2024-08-01 11:33:05 -0700

    new

--- portfolio/src/components/Home/Home.jsx
+++ portfolio/src/components/Home/Home.jsx
@@ -1,4 +1,7 @@
-import React from "react"
+import React from "react";
+import "./home.css";
+import Social from "./Social";
+
 
 const Home= () => {
   return (
@@ -6,7 +9,11 @@ const Home= () => {
       <section className="home section" id="home">
         <div className="home_container container grid">
             <div className="home_content grid">
-                
+                <Social />
+
+                <div className="home_img"></div>
+
+                <Data />
             </div>
         </div>
       </section>
--- portfolio/src/components/Home/Social.jsx
+++ portfolio/src/components/Home/Social.jsx
@@ -2,10 +2,20 @@ import React from "react"
 
 const Social = () => {
   return (
-    <div>
-      
+    <div className="home_social">
+      <a href ="" className="home_social-icon" target="_blank">
+      <i class="uil uil-instagram"></i>
+      </a>
+    
+      <a href ="" className="home_social-icon" target="_blank">
+      <i class="uil uil-dribble"></i>
+      </a>
+    
+      <a href ="https://github.com/" className="home_social-icon" target="_blank">
+      <i class="uil uil-github-alt"></i>
+      </a>
     </div>
   )
 };
 
-export default Soc
+export default Social;
